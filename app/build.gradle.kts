be5a3ed0278be77adca4fd3622621d57ac9e97ff
GongJu <hhwa39@gmail.com>
2023-08-24 15:41:19 +0900

    targetSdk 및 compileSdk version 34 로 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.sampleproject"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.sampleproject"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
